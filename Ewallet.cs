6fc296bc67a998ab9f0b9264f17c18923a161125
Raghu Prashanth <104619388+Raghuprashanth@users.noreply.github.com>
2022-09-28 21:40:44 +0530

    Add files via upload

---
+++ Ewallet.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EShopping_Wallet.Models
+{
+    public class Ewallet
+    {
+        [Key]
+        public int ewalletid { get; set; }
+        public float ewalletbalance { get; set; }
+        public int? Userid { get; set; }
+        [ForeignKey("Userid")]
+        public User? User { get; set; }
+    }
+}
---
+++ Merchantsales.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EShopping_Wallet.Models
+{
+    public class Marchent
+    {
+        [Key]
+        public int Salesid { get; set; }   
+        public float salestotalEarning { get; set; }
+        public int? Userid { get; set; }
+        [ForeignKey("Userid")]
+        public User? User { get; set; }
+
+    }
+}
